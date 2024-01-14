5ed64e57251215d4afc299d3bd476d25d96f3c79
F9Uf <atoseka@gmail.com>
2024-01-14 18:54:58 +0700

    completed lesson 6

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -1,4 +1,6 @@
 import requests
+import duckdb
+import os
 from . import constants
 from dagster import asset
 
@@ -15,6 +17,33 @@ def taxi_trips_file():
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
 
+@asset(
+	deps=["taxi_trips_file"]
+)
+def taxi_trips():
+    """
+        The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+            select
+                    VendorID as vendor_id,
+                    PULocationID as pickup_zone_id,
+                    DOLocationID as dropoff_zone_id,
+                    RatecodeID as rate_code_id,
+                    payment_type as payment_type,
+                    tpep_dropoff_datetime as dropoff_datetime,
+                    tpep_pickup_datetime as pickup_datetime,
+                    trip_distance as trip_distance,
+                    passenger_count as passenger_count,
+                    total_amount as total_amount
+            from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
+
 
 @asset
 def taxi_zones_file():
@@ -27,3 +56,21 @@ def taxi_zones_file():
 
     with open("data/raw/taxi_zones.csv", "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
+
+@asset(
+	deps=["taxi_zones_file"]
+)
+def taxi_zones():
+    sql_query = f"""
+        create or replace table zones as (
+            select
+                LocationID as zone_id,
+                zone,
+                borough,
+                the_geom as geometry
+            from '{constants.TAXI_ZONES_FILE_PATH}'
+        );
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
