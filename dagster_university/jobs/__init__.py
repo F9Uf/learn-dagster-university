86e5a4a4d029b99756bfe08dd984cf56462cac51
F9Uf <atoseka@gmail.com>
2024-02-01 00:35:06 +0700

    add partition to complete lesson 8

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -1,13 +1,16 @@
 from dagster import AssetSelection, define_asset_job
+from ..partitions import monthly_partitions, weekly_partitions
 
 trips_by_week = AssetSelection.keys(["trips_by_week"])
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
     selection=AssetSelection.all() - trips_by_week,
+    partitions_def=monthly_partitions,
 )
 
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
     selection=trips_by_week,
+    partitions_def=weekly_partitions,
 )
