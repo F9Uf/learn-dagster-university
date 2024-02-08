ce363e6e5fd8eb7f92961dcc078c6ba3f5269a2d
F9Uf <atoseka@gmail.com>
2024-02-08 23:59:35 +0700

    completed lesson9

--- dagster_university/jobs/__init__.py
+++ dagster_university/jobs/__init__.py
@@ -2,10 +2,11 @@ from dagster import AssetSelection, define_asset_job
 from ..partitions import monthly_partitions, weekly_partitions
 
 trips_by_week = AssetSelection.keys(["trips_by_week"])
+addhoc_request = AssetSelection.keys(["adhoc_request"])
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
-    selection=AssetSelection.all() - trips_by_week,
+    selection=AssetSelection.all() - trips_by_week - addhoc_request,
     partitions_def=monthly_partitions,
 )
 
@@ -13,4 +14,9 @@ weekly_update_job = define_asset_job(
     name="weekly_update_job",
     selection=trips_by_week,
     partitions_def=weekly_partitions,
+)
+
+addhoc_request_job = define_asset_job(
+    name="addhoc_request_job",
+    selection=addhoc_request,
 )
